97a97a4416eb79190ec7b14d9a57340811ce9c2e
GUDDU <GUDDU@DESKTOP-7DFHRST>
2022-08-17 05:04:54 -0700

    Added utility

---
+++ src/utility/Screenshot.java
@@ -0,0 +1,16 @@
+package utility;
+
+public class Screenshot {
+	
+	public void CaptureScreenshot()
+	{
+		System.out.println("Capturing screenshot at my computer location");
+		System.out.println("i have stored the data at c");
+		
+		
+	}
+	
+	
+	
+
+}
